d6907b15b7e92d7791eb53c6b6ddd1bd297208a1
tanim <tanim@DESKTOP-U4SAFQS.Dlink>
2022-11-30 11:34:24 +0530

    Commit

---
+++ C2TC_Training/src/c2tc/Demo.java
@@ -0,0 +1,15 @@
+package c2tc;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Welcome to core java 8");
+		System.out.println("Stremapi,collections");
+		int a,b;
+		a=10;
+		b=60;
+		System.out.println("Add"+ a + b);        //str+int+int so it concates
+		System.out.println("Add"+ (a + b));      //now a and b is added
+		System.out.println(a+b);
+	}
+
+}
---
+++ C2TC_Training/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tanim
+ *
+ */
+module C2TC_Training {
+}
